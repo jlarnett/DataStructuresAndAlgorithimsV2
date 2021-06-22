7bfe502d5628593bd34da55a83ce9109536e4ca6
Johnny Arnett <im10g@hotmail.com>
2021-06-21 22:22:20 -0400

    Add project files.

---
+++ DataStructuresAndAlgorithimsV2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataStructuresAndAlgorithimsV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nemo = {"nemo"};
+
+            FindNemo(nemo);
+        }
+
+        public static void FindNemo(string[] array)
+        {
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == "nemo")
+                {
+                    Console.WriteLine("Found Nemo");
+                }
+            }
+        }
+    }
+}
