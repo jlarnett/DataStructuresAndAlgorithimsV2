b6a9dc963aa5864dac78fd251b6a4dc190695974
Johnny Arnett <im10g@hotmail.com>
2021-07-01 20:11:29 -0400

    Added ArrayFunctionHolder class & StringFunctionHolder class to organize algorithms

--- DataStructuresAndAlgorithimsV2/Program.cs
+++ DataStructuresAndAlgorithimsV2/Program.cs
@@ -8,63 +8,6 @@ namespace DataStructuresAndAlgorithimsV2
     {
         static void Main(string[] args)
         {
-            //MergeArrays();
-            //HashTables();
-
-
-            //HashTable table = new HashTable(10);
-            //table.Set("grapes", 1000);
-            //table.Set("Apples", 50);
-            //table.Set("Gore", 30);
-
-
-            //foreach (var key in table.Keys())
-            //{
-            //    Console.WriteLine(key + ": " + table.Get(key));
-            //}
-
-
-            //int result = FunctionHolder.FirstRecurringElement(new[] {1, 5, 3, 10, 20, 4, 5, 3}).GetValueOrDefault();
-            //Console.WriteLine(result.ToString());
-
-            //FunctionHolder.RomanToInt("MCMXCIV");
-
-            int result = FunctionHolder.RemoveDuplicateElementsSortedArray(new[] {1, 2, 4, 5, 5, 6, 6, 7, 8, 10});
-            Console.WriteLine(result.ToString());
-
         }
-
-
-
-        static void MergeArrays()
-        {
-            int[] arr1 = {1, 4, 5, 10, 20};
-            int n1 = arr1.Length;
-
-            int[] arr2 = {1, 8, 9, 10, 20};
-            int n2 = arr2.Length;
-
-            int[] arr3 = new int[n1 + n2];
-
-            var returnArray = FunctionHolder.MergeSortedArrays(arr1, arr2, n1, n2, arr3);
-
-            for (int i = 0; i < returnArray.Length; i++)
-            {
-                Console.WriteLine(returnArray[i].ToString());
-            }
-        }
-
-
-        static void HashTables()
-        {
-            Hashtable newTable = new Hashtable();
-            newTable.Add("barry", 1);
-            newTable.Add("James", 1);
-
-
-            Console.WriteLine(newTable["barry"]);
-            Console.WriteLine(newTable["James"]);
-        }
-
     }
 }
---
+++ DataStructuresAndAlgorithimsV2/StringFunctionHolder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithimsV2
+{
+    public static class StringFunctionHolder
+    {
+        public static string ReverseString(string incomingString)
+        {
+            //Reverses string without converting to array directly.
+            //O(n)
+            string result = String.Empty;
+
+            for (int i = incomingString.Length - 1; i >= 0; i--)
+            {
+                result += incomingString[i];
+            }
+
+            return result;
+        }
+
+        public static string ReverseString2(string incomingString)
+        {
+            //Built in C# way to reverse strings after converting to array.
+            char[] chars = incomingString.ToCharArray();
+            Array.Reverse(chars);
+
+            return new string(chars);
+        }
+
+        public static int RomanToInt(string s)
+        {
+            Dictionary<char, int> RomanMap = new Dictionary<char, int>()
+            {
+                {'I', 1},
+                {'V', 5},
+                {'X', 10},
+                {'L', 50},
+                {'C', 100},
+                {'D', 500},
+                {'M', 1000}
+            };
+        
+            int number = 0;
+
+            //Loops through Roman numeral string
+            for (int i = 0; i < s.Length; i++)
+            {
+                //Checks whether the roman[i] value is < roman[i +1]. If it is it does the pre-subtraction before moving on to next roman numeral as normal..
+                if (i + 1 < s.Length && RomanMap[s[i]] < RomanMap[s[i + 1]])
+                {
+                    number -= RomanMap[s[i]];
+                }
+                else
+                {
+                    //If next number is smaller than it simply adds to total number
+                    number += RomanMap[s[i]];
+                }
+            }
+        
+            return number;
+        }
+    }
+}
